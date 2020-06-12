81f0b76847151ba2c1917ffce930e71bcf99b843
Nikita Denisov <nickdnsv@gmail.com>
2020-06-12 14:20:22 +0300

    minor refactoring

--- src/views/Edit/Edit.vue
+++ src/views/Edit/Edit.vue
@@ -2,18 +2,25 @@
   <div class="edit">
     <Popup :class="{showPopup: isPopupShown}" title="Note successfully saved!"></Popup>
 
-    <Confirm v-if="isLeaveConfirmOpened" title="Do you want leave this page?" @agreeAction="onCancelEditing"
-             @closeConfirm="isLeaveConfirmOpened=false"></Confirm>
-    <Confirm v-if="isDeleteConfirmOpened" title="Do you want delete this note?" @agreeAction="deleteNote"
-             @closeConfirm="isDeleteConfirmOpened=false"></Confirm>
+    <Confirm v-if="isLeaveConfirmOpened"
+             title="Do you want leave this page?"
+             @agreeAction="onCancelEditing"
+             @closeConfirm="isLeaveConfirmOpened = false"></Confirm>
 
-    <Form v-if="isTodoFormOpened" @closeForm="openTodoForm" @addItemInForm="addTodo($event)"></Form>
+    <Confirm v-if="isDeleteConfirmOpened"
+             title="Do you want delete this note?"
+             @agreeAction="deleteNote"
+             @closeConfirm="isDeleteConfirmOpened = false"></Confirm>
+
+    <Form v-if="isTodoFormOpened"
+          @closeForm="openTodoForm"
+          @addItemInForm="addTodo($event)"></Form>
 
     <form class="edit-form" @submit.prevent="saveNote">
       <div class="buttons-group buttons-group--interactions">
         <button type="button" class="button button--undo" @click="setPreviousState" :disabled="undoDisabled"></button>
         <button type="button" class="button button--redo" @click="setUpdatedState" :disabled="redoDisabled"></button>
-        <button type="button" class="button button--delete" @click="isDeleteConfirmOpened=true"></button>
+        <button type="button" class="button button--delete" @click="isDeleteConfirmOpened = true"></button>
       </div>
 
       <h3>Note Title</h3>
@@ -22,6 +29,7 @@
              @blur="onChangeEvents"
              class="input--title" type="text"
              v-model="actualNoteVersion.title">
+
       <hr/>
 
       <h3>Todos</h3>
@@ -33,6 +41,7 @@
         @deleteTodo="deleteTodo($event)"
         @setButtonsDisabling="onChangeEvents"
         :todo="todo" :key="todo.id"></TodoItem>
+
       <div class="buttons-group">
         <button class="button button--save">Save</button>
         <button type="button" class="button button--cancel" @click="isLeaveConfirmOpened=true">Cancel</button>
@@ -53,16 +62,20 @@
     data() {
       return {
         notes: [],
+
+        // 3 состояния для возможности возвращению/отмены изменений
         previousNoteVersion: {},
         updatedNoteVersion: {},
         actualNoteVersion: {},
 
+        // Состояния дополнительных окон
         isTodoFormOpened: false,
         isLeaveConfirmOpened: false,
         isDeleteConfirmOpened: false,
         stateButtonsDisabled: true,
         isPopupShown: false,
 
+        // Состояния кнопок возвращению/отмены изменений для лучшего UX
         undo: false,
         redo: true,
         undoDisabled: true,
@@ -70,35 +83,9 @@
       }
     },
     methods: {
-      onCancelEditing() {
-        this.$router.push('/');
-      },
       deleteNote() {
         localStorage.setItem('notes', JSON.stringify([...this.notes].filter(item => item.id !== this.updatedNoteVersion.id)));
         this.$router.push('/');
-      },
-      onChangeEvents() {
-        if (!this.redoDisabled) {
-          this.previousNoteVersion = {...this.updatedNoteVersion};
-          this.updatedNoteVersion = {...this.actualNoteVersion};
-          this.redoDisabled = true;
-          this.undoDisabled = false
-        }
-      },
-      setButtonsDisabling() {
-        if (this.undoDisabled && this.redoDisabled) {
-          this.undoDisabled = false;
-        }
-      },
-      setPreviousState() {
-        this.undoDisabled = true;
-        this.redoDisabled = false;
-        this.actualNoteVersion = this.previousNoteVersion
-      },
-      setUpdatedState() {
-        this.undoDisabled = false;
-        this.redoDisabled = true;
-        this.actualNoteVersion = this.updatedNoteVersion
       },
       openTodoForm() {
         this.isTodoFormOpened = !this.isTodoFormOpened
@@ -120,21 +107,6 @@
         this.previousNoteVersion = {...this.updatedNoteVersion};
         this.updatedNoteVersion = {...this.updatedNoteVersion, todos}
       },
-      setPreviousNoteVersion(todoId) {
-        this.setButtonsDisabling();
-        if (todoId) {
-          this.previousNoteVersion = {
-            ...this.updatedNoteVersion, todos: [...this.updatedNoteVersion.todos].map(item => {
-              if (item.id === todoId) {
-                return {...item}
-              }
-              return item
-            })
-          }
-        } else {
-          this.previousNoteVersion = {...this.updatedNoteVersion}
-        }
-      },
       deleteTodo(todoId) {
         if (!this.redoDisabled) {
           this.updatedNoteVersion = {...this.previousNoteVersion};
@@ -155,14 +127,60 @@
           }
           return item
         })));
+
+        //Вызов уведомления об удачном сохранении и удаление класса анимации через 3 секунды
         this.isPopupShown = true;
         setTimeout(() => this.isPopupShown = false, 3000);
+      },
+      onCancelEditing() {
+        this.$router.push('/');
+      },
+
+      //Функции управления состоянием кнопок возвращению/отмены изменений
+      onChangeEvents() {
+        if (!this.redoDisabled) {
+          this.previousNoteVersion = {...this.updatedNoteVersion};
+          this.updatedNoteVersion = {...this.actualNoteVersion};
+          this.redoDisabled = true;
+          this.undoDisabled = false
+        }
+      },
+      setButtonsDisabling() {
+        //Кнопка отмены изменения становится активной после изменения поля title заметки
+        if (this.undoDisabled && this.redoDisabled) {
+          this.undoDisabled = false;
+        }
+      },
+      setPreviousState() {
+        this.undoDisabled = true;
+        this.redoDisabled = false;
+        this.actualNoteVersion = this.previousNoteVersion
+      },
+      setUpdatedState() {
+        this.undoDisabled = false;
+        this.redoDisabled = true;
+        this.actualNoteVersion = this.updatedNoteVersion
+      },
+      setPreviousNoteVersion(todoId) {
+        this.setButtonsDisabling();
+        if (todoId) {
+          this.previousNoteVersion = {
+            ...this.updatedNoteVersion, todos: [...this.updatedNoteVersion.todos].map(item => {
+              if (item.id === todoId) {
+                return {...item}
+              }
+              return item
+            })
+          }
+        } else {
+          this.previousNoteVersion = {...this.updatedNoteVersion}
+        }
       },
     },
     mounted() {
       this.notes = JSON.parse(localStorage.getItem('notes'));
 
-      // Состояния заметки для возможности отмены изменений
+      // Состояния заметки для возможности отмены изменений при монтировании компоненты равны
       this.previousNoteVersion = JSON.parse(localStorage.getItem('notes')).filter(item => item.id === this.$route.params.id)[0];
       this.updatedNoteVersion = JSON.parse(localStorage.getItem('notes')).filter(item => item.id === this.$route.params.id)[0];
       this.actualNoteVersion = this.updatedNoteVersion;
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="notes__wrapper">
+
     <Form v-if="isNoteFormOpened" @closeForm="openNoteForm" @addItemInForm="addNote($event)"></Form>
 
     <Confirm title="Do you want delete this note?" v-if="isConfirmOpened" @closeConfirm="isConfirmOpened = false" @agreeAction="deleteNote"></Confirm>
@@ -8,6 +9,7 @@
 
     <div class="notes__container">
       <p v-if="notes.length === 0" style="grid-column: 1/4">You don't have notes. Let's create one!</p>
+
       <div v-for="note in notes" :key="note.id" class="note">
         <div class="note__header">
           <h3>{{note.title}}</h3>
@@ -16,15 +18,20 @@
             <button class="button button--delete" @click="showConfirmPop(note.id)"></button>
           </div>
         </div>
+
         <hr/>
+
         <div class="note__todos">
           <h4>Todos</h4>
+
           <p v-if="note.todos.length === 0">Add new todo in edit mode</p>
+
           <div v-else class="note__todos__items">
             <p v-for="todo in note.todos.slice(0, 2)" :key="todo.id">{{todo.title}}</p>
             <p style="text-align: center; font-size: 1.5rem; line-height: .85" v-if="note.todos.length > 2">...</p>
           </div>
         </div>
+
       </div>
     </div>
   </div>
@@ -49,15 +56,6 @@
       }
     },
     methods: {
-      showConfirmPop(noteId) {
-        this.isConfirmOpened = true;
-        this.noteId = noteId;
-      },
-      deleteNote() {
-        localStorage.setItem('notes', JSON.stringify([...this.notes].filter(item => item.id !== this.noteId)));
-        this.notes = [...this.notes].filter(item => item.id !== this.noteId);
-        this.isConfirmOpened = false;
-      },
       openNoteForm() {
         this.isNoteFormOpened = !this.isNoteFormOpened
       },
@@ -67,6 +65,15 @@
 
         localStorage.setItem('notes', JSON.stringify([newNote, ...this.notes]));
         this.notes = [newNote, ...this.notes];
+      },
+      deleteNote() {
+        localStorage.setItem('notes', JSON.stringify([...this.notes].filter(item => item.id !== this.noteId)));
+        this.notes = [...this.notes].filter(item => item.id !== this.noteId);
+        this.isConfirmOpened = false;
+      },
+      showConfirmPop(noteId) {
+        this.isConfirmOpened = true;
+        this.noteId = noteId;
       },
     },
     mounted() {
